9dee0f4b3d09a825b32c8b5f4cdd9cb6cedbe3dc
Kate <pikatese@gmail.com>
2017-11-07 21:20:06 +0200

    move

--- remonttilaina/js/main.js
+++ remonttilaina/js/main.js
@@ -6,4 +6,10 @@ $(document).ready(function(){
    		}
    		$("option").append( " €" );
 	});
+	$(".menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
 });
